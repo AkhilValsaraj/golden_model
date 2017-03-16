0fe9926cc4e54d7b304f5d260c6d0a32358d0fda
AkhilValsaraj <akhil0999@gmail.com>
2017-03-16 14:50:09 -0400

    added files

---
+++ decode.sv
@@ -0,0 +1,61 @@
+`define ADD 0001
+`define AND 
+`define NOT
+`define BR
+`define JMP
+`define LD
+`define LDR
+`define LDI
+`define LEA
+`define ST
+`define STR
+`define STI
+
+
+module decode(
+	input clock,
+	input reset,
+	input [15:0]	npc_in,
+	input enable_decode,
+	input [15:0]	instr_dout,
+	input [2:0]	psr,
+	output logic [15:0]	IR,
+	output logic [5:0]	E_control,
+	output logic [15:0]npc_out,
+	output logic Mem_Control,
+	output logic [1:0]	W_Control
+);
+
+always@(posedge clock)
+begin
+	if(!reset)
+	begin
+		IR	<=	0;
+		npc_out	<=	0;		
+		Mem_Control	<=	0;
+		W_Control	<=	0;
+		E_control	<=	0;
+	end
+	else
+		if(enable_decode)	//assign the decode logic
+		begin
+			IR <= Instr_dout;
+			npc_out<=npc_in;
+			Mem_Control	<=	mem_control;
+			W_Control	<=	w_control;
+			E_control	<=	e_control;
+		end
+end
+
+always@(*)
+begin
+
+	//W_Control
+	case(Instr_dout[15:12])
+	0001	:	
+	0101	:
+	1001	:
+	
+end
+
+endmodule
---
+++ fetch.sv
@@ -0,0 +1,29 @@
+module fetch(
+	input clock,
+	input reset,
+	input enable_updatePC,
+	input enable_fetch,
+	input [15:0] taddr,
+	input br_taken,
+	output logic [15:0] npc,
+	output logic [15:0] pc,
+	output logic instrmem_rd
+);
+
+assign npc = pc + 1;	//asynchronous npc
+
+always@(posedge clock)
+begin
+	if(!reset)	//reset synchronously
+		pc = 0;
+	else
+		pc = next_pc;
+end
+
+always@(*)
+begin
+	next_addr = (br_taken)? taddr: npc;	// choose branch
+	next_pc = (enable_updatePC)? next_addr: pc;		//choose to update
+end
+
+endmodule
