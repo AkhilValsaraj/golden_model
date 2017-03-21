565a63bb3ad2560d237d2d30005dc4c00c822496
AkhilValsaraj <akhil0999@gmail.com>
2017-03-21 17:03:26 -0400

    Edited fetch to include the interface signals

--- fetch.sv
+++ fetch.sv
@@ -1,31 +1,71 @@
-module fetch(
-	input clock,
-	input reset,
-	input enable_updatePC,
-	input enable_fetch,
-	input [15:0] taddr,
-	input br_taken,
-	output logic [15:0] npc,
-	output logic [15:0] pc,
-	output logic instrmem_rd
-);
+//TO DO: check the task calling and fork join and posedge
 
-logic [15:0] next_addr, next_pc;
+class Fetch_sb;
 
-assign npc = pc + 1;	//asynchronous npc
+	// //declaring inputs
+	// logic		enable_updatePC;
+	// logic		enable_fetch;
+	// logic	[15:0]	taddr;
+	// logic		br_taken;
 
-always@(posedge clock)
-begin
-	if(!reset)	//reset synchronously
-		pc = 0;
-	else
-		pc = next_pc;
-end
+	//declaring outputs
+	logic		instrmem_rd;
+	logic	[15:0]	pc;
+	logic	[15:0]	npc;
 
-always@(*)
-begin
-	next_addr = (br_taken)? taddr: npc;	// choose branch
-	next_pc = (enable_updatePC)? next_addr: pc;		//choose to update
-end
+	virtual LC3_io top_io;
+	virtual dut_Probe_if fetch_if;
 
-endmodule
+	function new(virtual LC3_io top_io, virtual dut_Probe_if fetch_if);
+		this.top_io = top_io;
+		this.fetch_if = fetch_if;	
+	endfunction
+
+	task reset;
+		pc = 16'h3000;
+		npc = pc + 1;
+		instrmem_rd = 1'b1;
+	endtask : reset
+
+	task run_fetch;
+		if(top_io.reset)
+		begin
+			reset();
+		end : if reset
+
+		else
+		begin
+			if(fetch_if.fetch_enable_fetch)
+			begin
+				instrmem_rd = 1'b1;
+			end
+			else
+			begin
+				instrmem_rd = 1'b0;
+			end
+			if(fetch_if.fetch_enable_updatePC)
+			begin
+				if(fetch_if.fetch_br_taken)
+				begin
+					pc = fetch_if.fetch_taddr;
+				end
+				else
+				begin
+					pc = pc+1;
+				end
+			end
+			npc = pc+1;
+		end : else block
+	endtask : run_fetch
+
+	task check_fetch;
+	//@(posedge top_io.clock)
+       		 if(instrmem_rd !== fetch_if.fetch_instrmem_rd)
+       			 $display($time,"BUG IN FETCH DUT instrmem_rd_DUT = %h | instrmem_rd = %h\n",fetch_if.fetch_instrmem_rd,instrmem_rd);	
+       		 if(pc !== fetch_if.fetch_pc)
+       			 $display($time,"BUG IN FETCH DUT pc_DUT = %h | pc = %h\n",fetch_if.fetch_pc,pc);	
+       		 if(npc !== fetch_if.fetch_npc)
+       			 $display($time,"BUG IN FETCH DUT npc_DUT = %h | npc = %h\n",fetch_if.fetch_npc,npc);	
+	endtask : check_fetch
+
+endclass
